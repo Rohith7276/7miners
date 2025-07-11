74d7232eabf7616bb9b13b4fe878dbbb5d3f60f5
Rohith H P <140833328+Rohith7276@users.noreply.github.com>
2025-07-11 21:44:11 +0530

    Update Bitaxe.jsx

--- src/pages/products/Bitaxe.jsx
+++ src/pages/products/Bitaxe.jsx
@@ -133,7 +133,7 @@ const Bitaxe = () => {
                                 </a>
 
                                 <a className='h-10 w-10 flex items-center justify-center' href="https://api.whatsapp.com/send?text=Check this out! https://7miners.in" target="_blank">
-                                    <img className='w-5 brightness-0  contrast-200 h-fit' src="https://imgs.search.brave.com/1_lYKyfLRXThu0LN4vcKf46xi0zCuhTZlf7k-fWGT6Q/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtMDAuaWNvbmR1/Y2suY29tL2Fzc2V0/cy4wMC93aGF0c2Fw/cC1pY29uLTI1Nngy/NTYtbGtpZzlybzEu/cG5n" alt="whatsapp" />
+                                    <img className='w-5 brightness-0  contrast-200 h-fit' src="https://images.seeklogo.com/logo-png/16/1/whatsapp-logo-png_seeklogo-168310.png" alt="whatsapp" />
                                 </a>
 
                                 <a className='h-10 w-10  flex items-center justify-center' href="https://www.linkedin.com/shareArticle?mini=true&url=https://7miners.in&title=Awesome%20Website" target="_blank">
